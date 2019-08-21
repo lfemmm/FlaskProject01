aae5b2d52bed1d7fa9798e55b4da54426f3772b3
lfemmm <783898950@qq.com>
2019-08-22 01:55:31 +0800

    ..

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
-# 从 flask 这个包中导入 Flask 这个类
-# Flask 这个类是项目的核心，以后很多操作都是基于这个类的对象
-# 注册 url、注册蓝图等都是基于这个类的对象
-from flask import Flask
+from flask import Flask, request
 
-# 创建一个 Flask 对象，传递__name__参数进去
-# __name__参数的作用：
-#   1、可以规定模板和静态文件的查找路径
-#   2、以后一些 Flask 插件，比如 Flask-migrate、Flask-SQLAlchemy 如果报错了，那么 Flask
-#      可以通过这个参数找到具体的错误位置。
 app = Flask(__name__)
 
-# @app.route：是一个装饰器
-# @app.route('/')就是讲 url 中的 / 映射到 hello_world 这个视图函数上面
-# 以后访问这个网站的 / 目录的时候，会执行 hello_world 这个函数，然后将这个函数的返回值，返回给浏览器。
-# http://127.0.0.1:5000/ ---> hello_world 函数
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/list/')
+def article_list():
+    print(1+1)
+    return '文章列表！！'
 
-# 如果这个文件是作为一个主文件运行，那么就执行 app.run() 方法
-# 也就是启动这个网站
+# 根据 id 查询
+@app.route('/article1/<article_id>/')
+def article_detail1(article_id):
+    return '你请求的文章编号是：%s' % article_id
+# 指定传参为整型，还可以 string,float...
+@app.route('/article2/<int:article_id>/')
+def article_detail2(article_id):
+    return '你请求的文章编号是：%s' % article_id
+
+# 和 string 类似，但是接受斜杠
+@app.route('/article/<path:test>/')
+def article_test(test):
+    return '这是多路径：%s' % test
+
+# 测试 uuid
+@app.route('/u/<uuid:user_id>/')
+def user_id(user_id):
+    return '用户的id是：%s' % user_id
+
+# any 指定多种路径
+# /blog/<id>/   或者  /user/<id>/
+@app.route('/<any(blog,user):url_path>/<id>/')
+def detail(url_path, id):
+    if url_path == 'blog':
+        return '博客的id是：%s' % id
+    if url_path == 'user':
+        return '用户的id是：%s' % id
+
+# 通过 ？ 形式传递参数
+#https://www.baidu.com/s?wd=uuid%E7%94%9F%E6%88%90
+@app.route('/d/')
+def d():
+    username = request.args.get('username')
+    psw = request.args.get('psw')
+    return '用户名是：%s'%username + '密码是：%s'%psw
+
 if __name__ == '__main__':
-    # app.run()：Flask 中的一个测试浏览器
-    # while True：
-    #   listen()
-    #   input()
     app.run()
