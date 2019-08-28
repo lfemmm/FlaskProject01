e3b95e7f4450200f55e51de71946f157c8190ae5
lifeng <783898950@qq.com>
2019-08-28 11:58:57 +0800

    2019-8-28

--- app.py
+++ app.py
@@ -24,12 +24,10 @@ def article_detail2(article_id):
 @app.route('/article/<path:test>/')
 def article_test(test):
     return '这是多路径：%s' % test
-
 # 测试 uuid
 @app.route('/u/<uuid:user_id>/')
 def user_id(user_id):
     return '用户的id是：%s' % user_id
-
 # any 指定多种路径
 # /blog/<id>/   或者  /user/<id>/
 @app.route('/<any(blog,user):url_path>/<id>/')
@@ -38,14 +36,17 @@ def detail(url_path, id):
         return '博客的id是：%s' % id
     if url_path == 'user':
         return '用户的id是：%s' % id
-
 # 通过 ？ 形式传递参数
 #https://www.baidu.com/s?wd=uuid%E7%94%9F%E6%88%90
 @app.route('/d/')
 def d():
-    username = request.args.get('username')
+    uname = request.args.get('uname')
     psw = request.args.get('psw')
-    return '用户名是：%s'%username + '密码是：%s'%psw
+    return '用户名是：%s'%uname + '密码是：%s'%psw
+
+
+#  url_for
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000,debug=True)
